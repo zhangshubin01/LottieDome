3be307f101ff73193391a52d300eb694fb7d5fb6
shb.zhang <shb.zhang@zuche.com>
2017-02-22 17:16:16 +0800

    添加 butterknife

--- app/src/main/java/com/example/zhangshubin/lottiedome/MainActivity.java
+++ app/src/main/java/com/example/zhangshubin/lottiedome/MainActivity.java
@@ -2,26 +2,30 @@ package com.example.zhangshubin.lottiedome;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
 
-//
-//    @BindView(R.id.animation_view)
-//    LottieAnimationView mAnimationView;
-//    @BindView(R.id.animation_view1)
-//    LottieAnimationView mAnimationView1;
-//    @BindView(R.id.activity_main)
-//    LinearLayout mActivityMain;
+
+    @BindView(R.id.animation_view)
+    LottieAnimationView mAnimationView;
+    @BindView(R.id.animation_view1)
+    LottieAnimationView mAnimationView1;
+    @BindView(R.id.activity_main)
+    LinearLayout mActivityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
-        LottieAnimationView mAnimationView1 = (LottieAnimationView) findViewById(R.id.animation_view1);
+        ButterKnife.bind(this);
+//        LottieAnimationView mAnimationView1 = (LottieAnimationView) findViewById(R.id.animation_view1);
         mAnimationView1.setAnimation("all.json");
         mAnimationView1.loop(true);
     }
