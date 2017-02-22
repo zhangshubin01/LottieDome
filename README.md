c462a2e974cd8dbf751aba2a38cefa1379afdb79
shb.zhang <shb.zhang@zuche.com>
2017-02-22 16:58:51 +0800

    上传

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# LottieDome
+# LottieDome  Lottie库的练习
+
+###1.编写过程中  遇到的问题
+>Error:Execution failed for task ':app:processDebugManifest'.
+>Manifest merger failed : uses-sdk:minSdkVersion 11 cannot be smaller than version 14 declared in library [com.airbnb.android:lottie:1.5.0] F:\CodeManagement\LottieDome\app\build\intermediates\exploded-aar\com.airbnb.android\lottie\1.5.0\AndroidManifest.xml
+>Suggestion: use tools:overrideLibrary="com.airbnb.lottie" to force usage
+
+  解决    minSdkVersion 给为14   最小支持14
+
+    defaultConfig {
+            applicationId "com.example.zhangshubin.lottiedome"
+            minSdkVersion 14
+            targetSdkVersion 25
+            versionCode 1
+            versionName "1.0"
+            testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner"
+      }
--- app/src/main/java/com/example/zhangshubin/lottiedome/MainActivity.java
+++ app/src/main/java/com/example/zhangshubin/lottiedome/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.zhangshubin.lottiedome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+//
+//    @BindView(R.id.animation_view)
+//    LottieAnimationView mAnimationView;
+//    @BindView(R.id.animation_view1)
+//    LottieAnimationView mAnimationView1;
+//    @BindView(R.id.activity_main)
+//    LinearLayout mActivityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+        LottieAnimationView mAnimationView1 = (LottieAnimationView) findViewById(R.id.animation_view1);
+        mAnimationView1.setAnimation("all.json");
+        mAnimationView1.loop(true);
     }
 }
